1dc4d6a4f797771a31efee534003977ad9b2d53e
Erick Mesquita Martins <erickmesquita319@gmail.com>
2022-07-09 10:49:19 -0300

    09/07
    Começando

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require('express')
 const mongoose = require("mongoose")
 const path = require("path")
+
 const app = express()
 
 app.set("views", path.join(__dirname, "assets/public"))
@@ -13,12 +14,10 @@ const routerSign = require("./router/routerSign")
 const routerAccount = require("./router/routerAccount")
 const routerRooms = require("./router/routerRooms")
 
-
 mongoose.connect(process.env.MONGODB_PATH,
     { useNewUrlParser: true, useUnifiedTopology: true},
     (error) => { if(error) throw error; console.log("Mongo Funcionando")
 })
-
 
 app.use("/login", express.json(), routerLogin)
 app.use("/sign", express.json(), routerSign)
